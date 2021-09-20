0ad01ad1013e36b8fcb16b75f698d02bdd84b558
rawan <73165337+RawanAF@users.noreply.github.com>
2021-09-20 13:42:17 +0300

    Add files via upload

---
+++ e-system/e-system-2vs/resources/js/js.js
@@ -0,0 +1,7 @@
+function darkFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+     document.getElementById("dark-toggle").value="الوضع الابيض"
+     
+ }
+
