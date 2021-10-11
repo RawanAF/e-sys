c1acea809d81a656f44c2267be747534c1200a62
razan1 <razanx1012@gmail.com>
2021-10-11 10:16:08 +0300

    add modal alert

--- e-system/e-system-2vs/resources/js/js.js
+++ e-system/e-system-2vs/resources/js/js.js
@@ -1,7 +1,48 @@
 function darkFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-     document.getElementById("dark-toggle").value="الوضع الابيض"
-     
- }
+    document.getElementById("dark-toggle").value = "الوضع الابيض"
 
+}
+// Alert Modal Type
+$(document).on('click', '#success', function (e) {
+    swal(
+        'تمت عملية الحفظ بنجاح',
+        '<b style="color:green;">Success</b>',
+        'success',
+
+    )
+});
+
+$(document).on('click', '#error', function (e) {
+    swal(
+        'Error!',
+        '<b style="color:red;">error</b>',
+        'error'
+    )
+});
+
+$(document).on('click', '#warning', function (e) {
+    swal(
+        'Warning!',
+        '<b style="color:coral;">warning</b>',
+        'warning'
+    )
+});
+
+$(document).on('click', '#info', function (e) {
+    swal(
+        'Info!',
+        '<b style="color:cornflowerblue;">info</b>',
+        'info'
+    )
+});
+
+$(document).on('click', '#question', function (e) {
+    swal(
+        'Question!',
+        '<b style="color:grey;">question</b>',
+        'question',
+
+    )
+});
