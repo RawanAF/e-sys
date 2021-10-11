c9829ceab81bd807a0090d7e7fbecefd58d7e069
RAWAN <ra14000ra@gmail.com>
2021-10-11 11:48:00 +0300

    Loader

--- e-system/e-system-2vs/resources/js/js.js
+++ e-system/e-system-2vs/resources/js/js.js
@@ -44,3 +44,12 @@ $('button').click(function () {
     })
 
 })
+
+
+
+/*  Loader */
+
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
