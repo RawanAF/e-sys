37f0a8e5535bf16b365ad30d9c024a0a13d57ab7
RAWAN <ra14000ra@gmail.com>
2021-10-14 12:54:09 +0300

    Loader

--- e-system/e-system-2vs/resources/js/js.js
+++ e-system/e-system-2vs/resources/js/js.js
@@ -50,7 +50,15 @@ $('button').click(function () {
 
 /*  Loader */
 
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
+function loadData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 2000);
+    })
+  }
+  
+  loadData()
+    .then(() => {
+      let preloaderEl = document.getElementById('preloader');
+      preloaderEl.classList.add('hidden');
+      preloaderEl.classList.remove('visible');
+    });
